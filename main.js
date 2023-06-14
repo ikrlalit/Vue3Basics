91bded48006c2e4bad3c978d4e2d0bf89f7f1f0a
kr-lalit <lalit.cse.bbk@gmail.com>
2023-06-14 19:20:03 +0530

    Added image part

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const app= Vue.createApp({
     data(){
         return{
             product: 'Socks',
-            productDesc:'Different color of quality socks are available',
+            image:'./assets/images/socks_green.jpg',
         }
     }
 })

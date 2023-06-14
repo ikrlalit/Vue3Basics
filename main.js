67ca78e7e8e215bf3ed1eb3e93ac0cdf3b2efa24
kr-lalit <lalit.cse.bbk@gmail.com>
2023-06-14 19:02:33 +0530

    Modified code now creating a product page

---
+++ main.js
@@ -0,0 +1,8 @@
+const app= Vue.createApp({
+    data(){
+        return{
+            product: 'Socks',
+            productDesc:'Different color of quality socks are available',
+        }
+    }
+})
